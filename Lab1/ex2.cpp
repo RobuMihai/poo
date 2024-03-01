2177f7433d1ae3ebf6452fee97caac946d933019
RobuMihai <mihairobu123@gmail.com>
2024-03-01 18:32:58 +0200

    corectare exercitiu 2

--- Lab1/ex2.cpp
+++ Lab1/ex2.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 using namespace std;
+
+void sort(char *cuvinte[],int nr)
+{
+    int i,j,l;
+    char *aux;
+    for(i=0;i<nr-1;i++)
+        for(j=i;j<nr;j++)
+        {
+            l=strlen(cuvinte[i])-strlen(cuvinte[j]);
+            if(l<0||l==0&&strcmp(cuvinte[i],cuvinte[j])<0)
+                {
+                    aux=cuvinte[i];
+                    cuvinte[i]=cuvinte[j];
+                    cuvinte[j]=aux;
+                }
+        }
+}
 
 int main()
 {
+    int nr=0;
+    int i;
+    char *cuvinte[100];
 	printf("Introduceti o propozitie: ");
-	char cuvant[100];
-	while (scanf("%s", cuvant))
-        {
-            printf("%s\n", cuvant);
-        }
+	char propozitie[100];
+    scanf("%[^\n]s", propozitie);//citeste pana intalneste caracterul \n (new line)
+    char *p=strtok(propozitie," ");
+    while(p!=NULL)
+    {
+        cuvinte[nr]=p;
+        nr++;
+        p=strtok(NULL," ");
+    }
 
-	printf("-----------------------------------------------------");
-	printf("\n\n");
+    sort(cuvinte,nr);
+
+	for(i=0;i<nr;i++)
+        printf("%s\n", cuvinte[i]);
+    return 0;
 }
